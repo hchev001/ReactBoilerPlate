cbe05b31425d0d6e9deb8ba1b4f7de16f2bd0bad
Hamilton Chevez <hchev001@fiu.edu>
2018-06-27 14:16:19 -0400

    updated webpack config to use devServer rules and also installed prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/appStyles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/appStyles.scss";
 
 const Index = () => {
-  return <div className="demo">
-    <h1 className="example">
-      Demo
-    </h1>!
-  </div>;
+  return (
+    <div className="demo">
+      <h1 className="example">Demo</h1>!
+    </div>
+  );
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,53 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: { main: './src/index.js'},
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 
-                {
-                    loader:"babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use:[
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({  // extracts css from javascript into own file
-            filename: 'style.[contenthash].css'
-        }),
-        new HtmlWebPackPlugin({
-            inject: false,
-            hash: true,
-            template: './src/index.html',
-            filename: 'index.html'
-        }),
-        new WebpackMd5Hash(),
-        new CleanWebpackPlugin('dist', {})
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // extracts css from javascript into own file
+      filename: "style.[contenthash].css"
+    }),
+    new HtmlWebPackPlugin({
+      inject: false,
+      hash: true,
+      template: "./src/index.html",
+      filename: "index.html"
+    }),
+    new WebpackMd5Hash(),
+    new CleanWebpackPlugin("dist", {})
+  ]
 };
